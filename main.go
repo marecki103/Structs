e5fa6f99004db75b2f8b7a8e705ad074fe16dd83
My <you@example.com>
2021-03-05 13:08:46 +0000

    Pointers

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func main() {
 
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
